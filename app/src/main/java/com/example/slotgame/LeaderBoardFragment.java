551810fbb0f4a14db516fee2ef5623b90924ee8b
calvinchen <calvinchen@msi.com>
2022-09-22 18:47:07 +0800

    1. Create images for add and minus bet, and create methods for those buttons to add or minus the different amounts of bet. 2. Create fragments to display the leaderboard and score list. And set methods to show or close those fragments in the option menu. 3. Design settings detail of bet setting and use these to design the program.

---
+++ app/src/main/java/com/example/slotgame/LeaderBoardFragment.java
@@ -0,0 +1,54 @@
+package com.example.slotgame;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Date;
+
+public class LeaderBoardFragment extends Fragment {
+    private static LeaderBoardFragment instance;
+    String name;
+    int score;
+    Date date;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.leader_board, container, false);
+    }
+
+    public static LeaderBoardFragment getInstance() {
+        if (instance == null)
+            instance = new LeaderBoardFragment();
+        return instance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
--- app/src/main/java/com/example/slotgame/MainActivity.java
+++ app/src/main/java/com/example/slotgame/MainActivity.java
@@ -16,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView score_t;
     private TextView bet_t;
     private FragmentManager option_board;
+    private FragmentManager leader_board;
+    private FragmentManager list_board;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +29,17 @@ public class MainActivity extends AppCompatActivity {
         refresh_score();
 
         option_board = getSupportFragmentManager();
+        leader_board = getSupportFragmentManager();
+        list_board = getSupportFragmentManager();
+
+
     }
 
     public void bar(View view) {
         Log.d(TAG, "bar: ");
         score.setRecord(7);
 
-        // later use
+        // TODO:
         // score.setRecord(score.getBet());
     }
 
@@ -72,6 +79,16 @@ public class MainActivity extends AppCompatActivity {
         refresh_score();
     }
 
+    public void minus_bet1(View view) {
+        Log.d(TAG, "minus_bet1: ");
+        refresh_score();
+        if (score.getBet() < 1)
+            return;
+
+        score.compute(-1);
+        refresh_score();
+    }
+
     public void add_bet5(View view) {
         Log.d(TAG, "add_bet5: ");
         refresh_score();
@@ -80,6 +97,15 @@ public class MainActivity extends AppCompatActivity {
         if (score.getBet() > 45)
             return;
         score.compute(5);
+        refresh_score();
+    }
+
+    public void minus_bet5(View view) {
+        Log.d(TAG, "add_bet5: ");
+        refresh_score();
+        if (score.getBet() < 5)
+            return;
+        score.compute(-5);
         refresh_score();
     }
 
@@ -94,28 +120,82 @@ public class MainActivity extends AppCompatActivity {
         refresh_score();
     }
 
+    public void minus_bet10(View view) {
+        Log.d(TAG, "add_bet10: ");
+        refresh_score();
+        if (score.getBet() < 10)
+            return;
+        score.compute(-10);
+        refresh_score();
+    }
+
     public void new_game(View view) {
         Log.d(TAG, "reset_game: ");
         score.init_score();
         refresh_score();
+        option_board(view);
     }
 
     public void option_board(View view) {
-        if (!OptionFragment.getInstance().isVisible()) {
-            Log.d(TAG, "option_board: open");
-            option_board.beginTransaction().add(R.id.board, OptionFragment.getInstance(), "option").commit();
-        }
-    }
-
-    public void close_option(View view) {
         if (OptionFragment.getInstance().isVisible()) {
             Log.d(TAG, "option_board: close");
             Fragment optionBoardFragment = getSupportFragmentManager().findFragmentByTag("option");
             if (optionBoardFragment != null) {
                 option_board.beginTransaction().remove(optionBoardFragment).commit();
             }
+        } else {
+            Log.d(TAG, "option_board: open");
+            option_board.beginTransaction().add(R.id.board, OptionFragment.getInstance(), "option").commit();
         }
     }
+
+    public void leader_board(View view) {
+        readLeaderBoardInfo();
+
+        if (LeaderBoardFragment.getInstance().isVisible()) {
+            Log.d(TAG, "leader_board: close");
+            Fragment leaderBoardFragment = getSupportFragmentManager().findFragmentByTag("leader");
+
+            if (leaderBoardFragment != null) {
+                leader_board.beginTransaction().remove(leaderBoardFragment).commit();
+            }
+        } else {
+            Log.d(TAG, "leader_board: open");
+            if (OptionFragment.getInstance().isVisible()) {
+                Log.d(TAG, "option_board: close");
+                Fragment optionBoardFragment = getSupportFragmentManager().findFragmentByTag("option");
+                if (optionBoardFragment != null) {
+                    option_board.beginTransaction().remove(optionBoardFragment).commit();
+                }
+            }
+            leader_board.beginTransaction().add(R.id.board, LeaderBoardFragment.getInstance(), "leader").commit();
+        }
+    }
+
+    public void list_board(View view) {
+        if (ListFragment.getInstance().isVisible()) {
+            Log.d(TAG, "list_board close");
+            Fragment listBoardFragment = getSupportFragmentManager().findFragmentByTag("list");
+            if (listBoardFragment != null) {
+                list_board.beginTransaction().remove(listBoardFragment).commit();
+            }
+        } else {
+            Log.d(TAG, "list: open list");
+            if (OptionFragment.getInstance().isVisible()) {
+                Log.d(TAG, "option_board: close");
+                Fragment optionBoardFragment = getSupportFragmentManager().findFragmentByTag("option");
+                if (optionBoardFragment != null) {
+                    option_board.beginTransaction().remove(optionBoardFragment).commit();
+                }
+            }
+            list_board.beginTransaction().add(R.id.board, ListFragment.getInstance(), "list").commit();
+        }
+    }
+
+    private void readLeaderBoardInfo() {
+        //TODO: read datebase
+    }
+
 
     public void quit(View view) {
         Log.d(TAG, "quit: go");
